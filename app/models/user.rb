7fa10f09574415fc0dccc8b57dc9926fddd08e5d
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-03 17:45:17 +0900

    バリデーション追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,17 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
+
+         with_options presence: true, do
+          validates :nickname, presence: true
+          validates :email, presence: true, unique: true, format: { with: (/@.+/) }
+          validates :encrypted_password, presence: true, length: {minimum: 6 }, format:{ with: (/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i), message: '６文字以上の半角英数字' }
+          validates :frist_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
+          validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
+          validates :frist_name_furigana, format: { with: /\A[ア-ケーー]+\z/, message: '全角カナを使用してください' }
+          validates :last_name_furigana, format: { with: /\A[ア-ケーー]+\z/, message: '全角カナを使用してください' }
+          validates :birthday
+         end
+  
 end
