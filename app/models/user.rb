742753785a1570d304cc2e3b14d5f35da0885d52
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-04 15:44:32 +0900

    バリーデーションの修正、新規登録及びログイン確認OK

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,12 @@ class User < ApplicationRecord
 
          with_options presence: true do
           validates :nickname
-          validates :encrypted_password, length: {minimum: 6 }, format:{ with: (/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i), message: '６文字以上の半角英数字' }
-          validates :frist_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
+          validates :email, uniqueness: true
+          validates :password, format:{ with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '６文字以上の半角英数字' }
+          validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
           validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
-          validates :frist_name_furigana, format: { with: /\A[ア-ケーー]+\z/, message: '全角カナを使用してください' }
-          validates :last_name_furigana, format: { with: /\A[ア-ケーー]+\z/, message: '全角カナを使用してください' }
+          validates :first_name_furigana, format: { with: /\A[ァ-ヶーー]+\z/, message: '全角カナを使用してください' }
+          validates :last_name_furigana, format: { with: /\A[ァ-ヶーー]+\z/, message: '全角カナを使用してください' }
           validates :birthday
          end
   
