b875ac8c973141a32d8dbd6c0f0fb93a28c38476
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-05 12:38:09 +0900

    items_controller生成、validates設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,16 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :price
+    validates :explanation
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_id
+    validates :shipping_area_id
+    validates :shipping_day_id
+    validates :user,              foreign_key: true
+  end
 end
