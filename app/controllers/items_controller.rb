8f79d4e43ee299e62452c71ab4b997cba539c9e0
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-05 19:23:49 +0900

    new.html修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,26 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+  
   def index
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :price, :explanation, :category_id, :status_id, :shipping_fee_id, :shipping_area_id, :shipping_day_id, :image).merge(user_id: current_user.id)
+  end
+
   
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,6 @@ class Item < ApplicationRecord
     validates :name, length: {maximum: 40 }
     validates :price, format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width character."}
     validates :explanation, length: {maximum: 1000 }
-    validates :user,              foreign_key: true
     validates :image
     with_options numericality: { other_than: 1 } do
       validates :category_id
@@ -22,5 +21,5 @@ class Item < ApplicationRecord
       validates :shipping_day_id
     end
   end
-  validates: price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to:9999999, message: "is out of setting range"}
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
 end
