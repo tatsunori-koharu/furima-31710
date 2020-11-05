9c46f1ba709a508fa7b393b0413b1d5aeb323014
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-05 23:09:59 +0900

    FactryBot作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    name                {Faker::Name.name}
+    price               {100000}
+    explanation         {Faker::Lorem.sentence}
+    image               {Faker::Lorem.sentence}
+    category_id         { 2 }
+    status_id           { 2 }
+    shipping_fee_id     { 2 }
+    shipping_area_id    { 2 }
+    shipping_day_id     { 2 }
+    association :user
   end
 end
