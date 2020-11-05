2f2f668554fbe8f93e17ce3eab5716a683d10a03
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-05 16:06:30 +0900

    activehash設定完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,9 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
+  belongs_to :shipping_fee
+  belongs_to :shipping_area
+  belongs_to :shipping_day
   belongs_to :user
   has_one_attached :image
 
