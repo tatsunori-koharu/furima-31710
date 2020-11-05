4155e2484d6892d75660a79a9b47275522733aef
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-06 00:46:37 +0900

    itemテストコード記述

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -13,46 +13,79 @@ RSpec.describe Item, type: :model do
     end
     context '新規出品がうまくいかないとき' do
       it 'nameが空だと登録できない' do
-        
+        @item.name = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
       end
-      it 'nameが40文字以上だと登録できない' do
-      
+      it 'nameが41文字以上だと登録できない' do
+        @item.name = "a" * 41
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name is too long (maximum is 40 characters)")
       end
       it 'priceが空だと登録できない' do
-      
+        @item.price = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
       end
       it 'prieが半角数字意外だと登録できない' do
-      
+        @item.price = "１５００"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is out of setting range")
       end
-      it 'priceが300円以下だと登録できない' do
-      
+      it 'priceが299円以下だと登録できない' do
+        @item.price = "299"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is out of setting range")
       end
-      it 'priceが1000000以上だと登録できない' do
-      
+      it 'priceが10000000以上だと登録できない' do
+        @item.price = "10000000"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is out of setting range")
       end
       it 'explanationが空だと登録できない' do
-      
+        @item.explanation = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explanation can't be blank")
       end
-      it 'explanationが1000文字以上だと登録できない' do
-      
+      it 'explanationが1001文字以上だと登録できない' do
+        @item.explanation = "a" * 1001
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explanation is too long (maximum is 1000 characters)")
       end
       it 'imageが空だと登録できない' do
-      
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
       end
       it 'category_idが「---」だと登録できない' do
-      
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
       it 'status_idが「---」だと登録できない' do
-      
+        @item.status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status must be other than 1")
       end
       it 'shipping_fee_idが「---」だと登録できない' do
-      
+        @item.shipping_fee_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee must be other than 1")
       end
       it 'shipping_area_idが「---」だと登録できない' do
-      
+        @item.shipping_area_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping area must be other than 1")
       end
       it 'shipping_day_idが「---」だと登録できない' do
-      
+        @item.shipping_day_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping day must be other than 1")
+      end
+      it 'userと紐づいていないと登録できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
       end
     end
   end
