53b1ee71e9e88d613488f9cfcde4257474bae6f7
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-08 20:59:40 +0900

    validates追加、READMEのstreet_addressテーブルをaddressテーブルへ変更

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,9 +3,9 @@ class OrderAddress
   attr_accessor :postal_code, :prefectures_id, :municipality, :address, :building_number, :phone_number
 
   with_options presence: true do
-    validates :postal_code
-    validates :prefectures_id, numericality: { other_than: 1 }
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefectures_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :municipality
     validates :address
-    validates :phone_number
+    validates :phone_number, format: { with: /\A[0-9]{11}+\z/ }
   end
