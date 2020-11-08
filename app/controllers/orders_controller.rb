6d08fcd61fbde9ce8e371574a6bf66b263fe23e8
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-08 17:25:02 +0900

    order,html記述、ルーティング設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,18 @@
 class OrdersController < ApplicationController
+  def index
+    @order = Order.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @order = Order.new(order_params)
+  end
+  
+
+  private
+  def order_params
+    params.require(:order).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'items#index'
   devise_for :users
-  resources :items, only: [:new, :create, :show]
-  resources :orders, only: [:index, :create]
+  resources :items, only: [:new, :create, :show] do
+    resources :orders, only: [:index, :create]
+  end
 end
