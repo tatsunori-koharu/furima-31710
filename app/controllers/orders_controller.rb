6ba2dd21755d03f274ed4e38ad57fe15a11c6366
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-08 19:25:07 +0900

    orderモデル,atter_accessor記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,20 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  # before_action :move_to_index
   def index
+    # binding.pry
     @order = Order.new
     @item = Item.find(params[:item_id])
+    # @user = User.find(params[:user_id])
   end
 
   def create
     @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
   end
   
 
@@ -13,6 +22,13 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order).merge(user_id: current_user.id, item_id: params[:item_id])
   end
+
+  # def move_to_index
+  #   binding.pry
+  #   if current_user.id == @item.user_id
+  #     render :root_path
+  #   end
+  # end
 end
 
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefectures_id, :municipality, :address, :building_number, :phone_number
   belongs_to :user
   belongs_to :item
 end
