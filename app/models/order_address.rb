56e593f4d8cccc446160999ebd1fe001da9beff4
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-09 15:56:11 +0900

    exsample記述

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,12 +5,11 @@ class OrderAddress
   with_options presence: true do
     validates :token
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefectures_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :municipality
     validates :address
     validates :phone_number, format: { with: /\A[0-9]{11}+\z/ }
   end
-
+  validates :prefectures_id, numericality: { other_than: 0, message: "can't be blank" }
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
     Address.create(postal_code: postal_code, prefectures_id: prefectures_id, municipality: municipality, address: address, building_number: building_number, phone_number: phone_number, order_id: order.id)
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe OrderAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @order_address = FactoryBot.build(:order)
+  end
+
+  describe '商品の購入' do
+    context '商品の購入がうまくいくとき' do
+      it '全ての情報を入力したとき' do
+        expect(@order_address).to be_valid
+      end
+      it 'building_numberが空でも購入できる' do
+      end
+    end
+    context '商品の購入がうまくいかないとき' do
+      it 'カード番号が空のとき' do
+      end
+      it 'exp_monthが空のとき' do
+      end
+      it 'exp_yearが空のとき' do
+      end
+      it 'cvcが空のとき' do
+      end
+      it 'postal_codeが空のとき' do
+      end
+      it 'postal_codeに（ー）がないとき' do
+      end
+      it 'prefectures_idが（０）のとき' do
+      end
+      it 'municipalityが空のとき' do
+      end
+      it 'addressが空のとき' do
+      end
+      it 'phone_numberが空のとき' do
+      end
+      it 'phone_numberが半角数字ではないとき' do
+      end
+      it '' do
+      end
+      it '' do
+      end
+      it '' do
+      end
+      it '' do
+      end
+    end
+  end
 end
