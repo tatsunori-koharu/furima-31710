e793ed89c7f325638a69f01382dc1c70a409678e
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-10 14:42:54 +0900

    コントローラ修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,8 +29,8 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.create(item_params)
-    if @item.save
+    item = Item.find(params[:id])
+    if item.update(item_params)
       redirect_to root_path
     else
       render :edit
