f09a8189e82db6187d87b320b14a285934fee424
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-15 17:39:41 +0900

    preview.js修正

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,22 +1,33 @@
 if (document.URL.match( /new/) || document.URL.match( /edit/ )) {
   document.addEventListener('DOMContentLoaded', function(){
     const ImageList = document.getElementById('image-area');
-    document.getElementById('item-image').addEventListener('change', function(e){
-      const file = e.target.files[0];
-      const blob = window.URL.createObjectURL(file);
 
-      //画像を表示させるためのdiv要素生成
+    //選択した画像を表示
+    const createImageHTML = (blob) => {
+      //画像表示のdiv生成
       const imageElement = document.createElement('div');
 
-      //表示する画像生成
+      //表示する画像を生成
       const blobImage = document.createElement('img');
       blobImage.width = 300;
       blobImage.height = 180;
       blobImage.setAttribute('src', blob);
-
       //生成したHTMLの要素をブラウザに表示させる
       imageElement.appendChild(blobImage);
       ImageList.appendChild(imageElement);
+    };
+
+    document.getElementById('item-image').addEventListener('change', function(e){
+      //画像が表示されていれれば既存を削除
+      const imageContent = document.querySelector('img');
+      if (imageContent){
+        imageContent.remove();
+      }
+
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+
+      createImageHTML(blob);
     });
   });
 }
