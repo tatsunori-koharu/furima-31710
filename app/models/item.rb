41e7979dd959846d37e68c7f06494a79578133d5
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-16 12:05:44 +0900

    ja.yml作成・記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,16 +11,16 @@ class Item < ApplicationRecord
 
   with_options presence: true do
     validates :name, length: { maximum: 40 }
-    validates :price, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width character.' }
+    validates :price, format: { with: /\A[0-9]+\z/, message: 'は半角英数字でご入力ください' }
     validates :explanation, length: { maximum: 1000 }
     validates :image
   end
-  with_options numericality: { other_than: 1 } do
+  with_options numericality: { other_than: 1, message: "[---]意外をお選びください" } do
     validates :category_id
     validates :status_id
     validates :shipping_fee_id
     validates :shipping_area_id
     validates :shipping_day_id
   end
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'は範囲内でご入力ください' }
 end
