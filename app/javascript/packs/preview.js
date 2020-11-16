24932b2cdbf5aaef6c19de9919e0863ddbac9867
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-16 09:33:43 +0900

    出品のプレビュー完成

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -6,24 +6,26 @@ if (document.URL.match( /new/) || document.URL.match( /edit/ )) {
     const createImageHTML = (blob) => {
       //画像表示のdiv生成
       const imageElement = document.createElement('div');
-
+      const nameElement = document.createElement('name')
       //表示する画像を生成
       const blobImage = document.createElement('img');
       blobImage.width = 300;
       blobImage.height = 180;
       blobImage.setAttribute('src', blob);
       //生成したHTMLの要素をブラウザに表示させる
-      imageElement.appendChild(blobImage);
+      nameElement.appendChild(blobImage);
+      imageElement.appendChild(nameElement);
       ImageList.appendChild(imageElement);
     };
 
     document.getElementById('item-image').addEventListener('change', function(e){
       //画像が表示されていれれば既存を削除
-      const imageContent = document.querySelector('img');
+      const imageContent = document.querySelector('name');
+      console.log(imageContent)
       if (imageContent){
         imageContent.remove();
       }
-
+      
       const file = e.target.files[0];
       const blob = window.URL.createObjectURL(file);
 
