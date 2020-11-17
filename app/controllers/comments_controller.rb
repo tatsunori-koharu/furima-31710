a296eb23cb209d54c7cfd1d649e6e6718cc7818c
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-17 09:37:56 +0900

    comment.channel生成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,15 @@
 class CommentsController < ApplicationController
   def new
+    @comments = Comment.all 
+    @comment = Comment.new
+  end
+
+  def create 
+    @comment = Comment.new(comment_params)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id] )
   end
 end
