aee08cbc3dd7240565e31251c55f0750de982299
tatsunori-koharu <tats208919ch@gmail.com>
2020-11-18 19:10:10 +0900

    修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,16 +1,14 @@
-if (document.URL.match( /new/) {
-  window.addEventListener('load', () => {
-    const priceInput = document.getElementById("item-price");
-    priceInput.addEventListener("input", () => {
-      const inputValue = priceInput.value;
-      const TAX = 0.1;
-      const TaxFee = Math.floor(inputValue * TAX); 
-      const addTaxDom = document.getElementById("add-tax-price");
-      addTaxDom.innerHTML = TaxFee;
-      const Profit = 0.9;
-      const ProfitFee = Math.floor(inputValue * Profit);
-      const addProfitDom = document.getElementById("profit");
-      addProfitDom.innerHTML = ProfitFee;
-    });
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const TAX = 0.1;
+    const TaxFee = Math.floor(inputValue * TAX); 
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = TaxFee;
+    const Profit = 0.9;
+    const ProfitFee = Math.floor(inputValue * Profit);
+    const addProfitDom = document.getElementById("profit");
+    addProfitDom.innerHTML = ProfitFee;
   });
-}
+});
